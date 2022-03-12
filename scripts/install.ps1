072ead2c85b3b4c4ad2e064efdf574ee5d1d2e77
Horizon <86346510+Horizon3833@users.noreply.github.com>
2022-03-12 10:47:15 +0530

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -14,9 +14,9 @@ function Create-Directory {
 }
 
 Write-Host Starting DevApps installation...
-$appengine=(Invoke-RestMethod "https://devapps-f239e-default-rtdb.firebaseio.com/gclousdk.json")
-$build=(Invoke-RestMethod "https://devapps-f239e-default-rtdb.firebaseio.com/build.json")
-$buildserver=(Invoke-RestMethod "https://devapps-f239e-default-rtdb.firebaseio.com/buildserver.json")
+$appengine="https://devapps-f239e-default-rtdb.firebaseio.com/gclousdk.json"
+$build="https://devapps-f239e-default-rtdb.firebaseio.com/build.json"
+$buildserver="https://devapps-f239e-default-rtdb.firebaseio.com/buildserver.json"
 $bindir="$HOME\.devapps\bin"
 $appdata="$env:APPDATA\devapps\"
 $winexe="https://devapps-f239e-default-rtdb.firebaseio.com/exe.json"
