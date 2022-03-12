60df1b40e887c35f38353ffcd301af7bd00432a9
Horizon3833 <horizon3833@gmail.com>
2022-03-12 10:38:28 +0530

    init

---
+++ scripts/install.ps1
@@ -0,0 +1,71 @@
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+function Create-Directory {
+  param (
+        $DirectoryName
+    )
+    if (-not (Test-Path -Path $DirectoryName)) {
+      New-Item -ItemType directory -Path $DirectoryName
+    }
+}
+
+Write-Host Starting DevApps installation...
+$appengine=(Invoke-RestMethod "https://devapps-f239e-default-rtdb.firebaseio.com/gclousdk.json")
+$build=(Invoke-RestMethod "https://devapps-f239e-default-rtdb.firebaseio.com/build.json")
+$buildserver=(Invoke-RestMethod "https://devapps-f239e-default-rtdb.firebaseio.com/buildserver.json")
+$bindir="$HOME\.devapps\bin"
+$appdata="$env:APPDATA\devapps\"
+$winexe="https://devapps-f239e-default-rtdb.firebaseio.com/exe.json"
+
+Create-Directory $appdata
+Create-Directory $appdata\deps
+Create-Directory $appdata\scripts
+Create-Directory $bindir
+
+Write-Host Downloading DevApps Executable
+Invoke-WebRequest $winexe -OutFile "$bindir\devapps.exe"
+Write-Host -ForegroundColor Green Done!
+Write-Host Downloading Storage SDK...
+Invoke-WebRequest $appengine -OutFile "$appdata\deps\appengine.zip"
+Write-Host -ForegroundColor Green Done!
+Write-Host Downloading Build Files...
+Invoke-WebRequest $build -OutFile "$appdata\deps\build.zip"
+Write-Host -ForegroundColor Green Done!
+Write-Host Downloading BuildServer...
+Invoke-WebRequest $buildserver -OutFile "$appdata\deps\buildserver.zip"
+Write-Host -ForegroundColor Green Done!
+Write-Host Donwloading Update Script...
+Invoke-WebRequest $update -OutFile "$appdata\scripts\upgrade.sh"
+Write-Host -ForegroundColor Green Done!
+Write-Host Extracting Files
+
+if (Test-Path $appdata\deps\build) {
+    Remove-Item -Recurse $appdata\deps\build
+}
+if (Test-Path $appdata\deps\appengine) {
+    Remove-Item -Recurse $appdata\deps\appengine
+}
+if (Test-Path $appdata\deps\buildserver) {
+    Remove-Item -Recurse $appdata\deps\buildserver
+}
+Unzip "$appdata\deps\build.zip" "$appdata\deps\build"
+Unzip "$appdata\deps\appengine.zip" "$appdata\deps\appengine"
+Unzip "$appdata\deps\buildserver.zip" "$appdata\deps\buildserver"
+Remove-Item "$appdata\deps\build.zip"
+Remove-Item "$appdata\deps\appengine.zip"
+Remove-Item "$appdata\deps\buildserver.zip"
+Write-Host -ForegroundColor Green Done!
+$env:Path += ";$bindir;"
+
+Write-Host -ForegroundColor Yellow DevApps has been successfully installed at $bindir\devapps.exe!
+
+Write-Host -ForegroundColor Green Thank You!
+
+
+
+
+
---
+++ scripts/install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+appdata="$APPDATA/devapps/"
+bindir="$HOME/.appzard/bin"
+
+function createDirIfDoesntExist {
+    if [ ! -d "$1" ]; then
+      mkdir "$1"
+    fi
+}
+function downloadAppzardExecutable {
+  executableURL="https://devapps-f239e-default-rtdb.firebaseio.com/exe.json"
+    curl --location \
+    --progress-bar \
+    --create-dirs \
+    --url "$executableURL" \
+    --output "${bindir}/devapps.exe"
+}
+function downloadAppengine {
+  curl --location \
+    --progress-bar \
+    --url "$1" \
+    --output "${appdata}/deps/appengine.zip"
+}
+function downloadBuildFiles {
+  curl --location \
+    --progress-bar \
+    --url "$1" \
+    --output "${appdata}/deps/build.zip"
+}
+function downloadBuildserverFiles {
+  curl --location \
+    --progress-bar \
+    --url "$1" \
+    --output "${appdata}/deps/buildserver.zip"
+}
+function downloadUpgradeScript() {
+    curl --location \
+    --progress-bar \
+    --url "" \
+    --output "${appdata}/scripts/upgrade.sh"
+}
+function unpackFiles {
+    # Unzip the downloaded files
+    unzip -o -q "${appdata}/deps/appengine.zip" -d "${appdata}/deps/appengine"
+    unzip -o -q "${appdata}/deps/build.zip" -d "${appdata}/deps"
+    unzip -o -q "${appdata}/deps/buildserver.zip" -d "${appdata}/deps/buildserver"
+    # So we don't take a large space
+    rm "${appdata}/deps/appengine.zip"
+    rm "${appdata}/deps/build.zip"
+    rm "${appdata}/deps/buildserver.zip"
+}
+green="\033[32m"
+yellow="\033[33m"
+reset="\033[0m"
+
+echo "Starting DevApps installation.."
+
+appengineDownloadUrl=$(curl -s "https://devapps-f239e-default-rtdb.firebaseio.com/gclousdk.json" | sed "s/\"//g")
+buildDownloadUrl=$(curl -s "https://devapps-f239e-default-rtdb.firebaseio.com/build.json" | sed "s/\"//g")
+buildserverDownloadUrl=$(curl -s "https://devapps-f239e-default-rtdb.firebaseio.com/buildserver.json" | sed "s/\"//g")
+
+createDirIfDoesntExist "$HOME/.devapps"
+createDirIfDoesntExist "${bindir}"
+createDirIfDoesntExist "${appdata}"
+createDirIfDoesntExist "${appdata}/deps"
+createDirIfDoesntExist "${appdata}/scripts"
+echo "Downloading DevApps Executable.."
+downloadAppzardExecutable
+chmod +x "$bindir/devapps.exe"
+
+echo -e "${green}Done!${reset}"
+echo "Downloading Appengine java SDK.."
+downloadAppengine "${appengineDownloadUrl}"
+echo -e "${green}Done!${reset}"
+echo "Downloading Build files.."
+downloadBuildFiles "${buildDownloadUrl}"
+echo -e "${green}Done!${reset}"
+echo "Downloading Buildserver files.."
+downloadBuildserverFiles "${buildserverDownloadUrl}"
+echo -e "${green}Done!${reset}"
+echo "Downloading Upgrade Script.."
+downloadUpgradeScript
+echo -e "${green}Done!${reset}"
+echo "Extracting files.."
+unpackFiles
+echo -e "${green}Done!${reset}"
+echo -e "${yellow}DevApps has been successfully installed on your device! Please add this path: ${bindir} to your PATH environment variable, then run devapps -v to verify the installation.${reset}"
